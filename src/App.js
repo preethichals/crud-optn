d3523d3673e84a985bde92985b5fc87cb5aa803b
preethichals <preethi.lyn@gmail.com>
2023-01-03 12:35:26 +0530

    adding router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
 
-import './App.css';
-import Card from './components/Card';
-import Header from './components/Header';
-
+import "./css/sb-admin-2.css";
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <div class=" flex">
-  <div class="w-13 bg-gray-400 h-12"><Card></Card></div>
-  <div class="w-13 bg-gray-500 h-12"><Card></Card></div>
-  <div class="w-13 bg-gray-400 h-12 float-right"><Card></Card></div>
-</div>
-      
-        
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar></Sidebar>
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Navbar></Navbar>
+            <Routes>
+              <Route path="/Student" element={<Student/>}></Route>
+            </Routes>
+          </div>
         </div>
-      
-    
+      </div>
+    </BrowserRouter>
   );
 }
 
